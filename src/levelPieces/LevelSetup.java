2b93956a700c17aa05b8fff9091000d6a891c4f3
Aiden Zeigler <118232891+iamziden@users.noreply.github.com>
2026-09-22 16:26:56 -0600

    Functionality working

---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,55 @@
+package levelPieces;
+
+import java.util.ArrayList;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.Moveable;
+
+public class LevelSetup {
+    private Drawable[] board;
+    private ArrayList<Moveable> movingPieces;
+    private ArrayList<GamePiece> interactingPieces;
+    private int playerStartLoc;
+
+    public void createLevel(int levelNum) {
+        board = new Drawable[GameEngine.BOARD_SIZE];
+        movingPieces = new ArrayList<>();
+        interactingPieces = new ArrayList<>();
+        playerStartLoc = GameEngine.BOARD_SIZE / 2;
+
+        if (levelNum == 1) {
+            //addPiece(new gamePiece(), 1);
+        }
+
+    }
+
+    public void addPiece(Drawable piece, int location) {
+        board[location] = piece;
+
+        if (piece instanceof GamePiece) {
+            interactingPieces.add((GamePiece) piece);
+        }
+
+        if (piece instanceof Moveable) {
+            movingPieces.add((Moveable) piece);
+        }
+    }
+
+    public Drawable[] getBoard() {
+        return board;
+    }
+
+    public ArrayList<Moveable> getMovingPieces() {
+        return movingPieces;
+    }
+
+    public ArrayList<GamePiece> getInteractingPieces() {
+        return interactingPieces;
+    }
+
+    public int getPlayerStartLoc() {
+        return playerStartLoc;
+    }
+
+}
